27197bea0cfda44c8a511e30f04b320b5127e024
lizkagr <artur.demianenko@stud.techin.lt>
2024-11-05 11:37:45 +0200

    add

---
+++ tinklapiu-kurimas/14-diena/Šakotieji algoritmai/JS/taskfour.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const a = parseInt(prompt("Enter the start of the interval:"));
+const b = parseInt(prompt("Enter end of interval:"));
+
+let count = 0;
+for (let i = a; i <= b; i++) {
+  if (i % 6 === 0) {
+    count++;
+  }
+}
+5;
+console.log(count);ё
